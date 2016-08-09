4564b1dc6b8adcb09bd0dd571939dcc00dbc313b
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-08-09 12:40:31 +0400

    fix tsumogiri detection

--- modules/actions.js
+++ modules/actions.js
@@ -1,11 +1,13 @@
-export const TSUMOGIRI_KAMICHA = 'E'
-export const TSUMOGIRI_TOIMEN = 'F'
-export const TSUMOGIRI_SHIMOCHA = 'G'
+export const TSUMOGIRI_KAMICHA = 'e'
+export const TSUMOGIRI_TOIMEN = 'f'
+export const TSUMOGIRI_SHIMOCHA = 'g'
 
-export const TEDASHI_KAMICHA = 'e'
-export const TEDASHI_TOIMEN = 'f'
-export const TEDASHI_SHIMOCHA = 'g'
+export const TEDASHI_KAMICHA = 'E'
+export const TEDASHI_TOIMEN = 'F'
+export const TEDASHI_SHIMOCHA = 'G'
 
+export const PLAYER_TAKE = 'T'
 export const PLAYER_DISCARD = 'D'
 
 export const INIT = 'INIT'
+export const DORA = 'DORA'
