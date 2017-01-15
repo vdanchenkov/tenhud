f4c63dc9a09060a7c003252565664ad97746056f
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2017-01-16 01:30:45 +0400

    svgs for tiles

--- modules/view/Tile.js
+++ modules/view/Tile.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const suitSymbols =  '東南西北白發中'
-const digitSymbols = '１２３４５６７８９'
-
-const Tile = ({tile}) => {
+const getImg = (tile) => {
   const type = tile / 4 | 0
   const suit = type / 9 | 0
   const number = type % 9
-  return suit === 0 ? <span style={{color: 'red'}}>{digitSymbols[number]}</span> :
-    suit === 1 ? <span style={{color: 'blue'}}>{digitSymbols[number]}</span> :
-    suit === 2 ? <span style={{color: 'green'}}>{digitSymbols[number]}</span> :
-    <span>{suitSymbols[number]}</span>
+
+  return suit === 0 ? `Man${number + 1}`:
+        suit === 1 ? `Pin${number + 1}` :
+        suit === 2 ? `Sou${number + 1}` :
+        ['Ton', 'Nan', 'Shaa', 'Pei', 'Haku', 'Hatsu', 'Chun'][number]
 }
+
+const Tile = styled.div`
+  display: inline-block;
+  width: 30px;
+  height: 40px;
+  background-position: center, center;
+  background-image: url(tile-images/${props => getImg(props.tile)}.svg), url(tile-images/front.svg);
+  background-repeat: no-repeat, no-repeat;
+  background-size: 27px 37px, 29px 39px;
+`
 
 export default Tile
